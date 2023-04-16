601a1363bf077d46e6e12d3a1fa6e151e9a7d7ca
Alan Ponsonby <52248011+PenguinRevolutionYT@users.noreply.github.com>
2023-04-15 22:13:08 -0500

    Add files via upload

---
+++ launch.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+
+#!/usr/bin/env sh
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -x polybar >/dev/null; do sleep 1; done
+
+# Launch
+polybar mainbar-i3 &
+
+echo "Bar launched..."
